c4d29303045d8720066b152d5315ca5203dcd0a0
Vinoth Kumar <vinothkumar281019@gmail.com>
2023-01-26 14:28:16 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
-// import { AuthContextProvider } from "../context/authContext";
-// import FlowDecider from "../Components/FlowDecider";
+import { AuthContextProvider } from "../context/authContext";
+import FlowDecider from "../Components/FlowDecider";
 import "./style/style.css"
 
 function App() {
   return (
-    <div>
-      what's wrong
-    </div>
-    // <AuthContextProvider>
-    //   <FlowDecider/>
-    // </AuthContextProvider>
+    <AuthContextProvider>
+      <FlowDecider/>
+    </AuthContextProvider>
   );
 }
 
