6f7f19eb472b047d879dc65eec8ab1e1fd6cd1af
Vinoth Kumar V <123581387+VinoV1999@users.noreply.github.com>
2023-12-22 21:57:19 +0530

    Update Amount.js
    bug in amt cal fixed

--- src/Functions/Amount.js
+++ src/Functions/Amount.js
@@ -3,7 +3,7 @@ class AmountCalculator {
     getAmountFromTime = (amountPerHour, time) => {
 
         const [hr,min,sec] = time.split(' : ');
-        const totalAmount = parseFloat(amountPerHour * parseInt(hr) + (parseFloat(parseInt(min) / 60 * 100).toFixed(2) / 100 ) * amountPerHour + (parseFloat(parseInt(sec) / 60 * 100).toFixed(2) / 100 ) * (1 / 60 * 100) * amountPerHour).toFixed(2);
+        const totalAmount = parseFloat(amountPerHour * parseInt(hr) + (parseFloat(parseInt(min) / 60 * 100).toFixed(2) / 100 ) * amountPerHour + (parseFloat(parseInt(sec) / 60 * 100).toFixed(2) / 100 ) * (1 / 60 * 100) / 100  * amountPerHour).toFixed(2);
         const INR = new Intl.NumberFormat('en-US', {
             style: 'currency',
             currency: 'INR',
